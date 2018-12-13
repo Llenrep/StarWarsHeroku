a3393f0910b4a5f66ac305b39178464852ac30be
Llenrep <lppernell1998@gmai.com>
2018-12-13 12:49:00 -0500

    fixed the server.js file

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+
+//Declare port for server Environmental variable or 8080 for local Development
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
